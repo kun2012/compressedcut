cbc4252bb439e84eaef951dbf29b375c87d6a746
Kun <nju.kun@gmail.com>
2015-03-07 15:20:33 +0800

    Hypercut not works fine.

--- krun.sh
+++ krun.sh
@@ -2,6 +2,26 @@
 INPUT_DIR="filters"
 OUTPUT_DIR="output"
 
+#Hypercut -->not works fine
+#binth=16
+#spfac=8
+#hypercuts=1
+#compressionON=0
+#binningON=0
+#mergingON=0
+#fineOn=0
+#ruleMoveUp=15
+
+#Efficuts
+binth=16
+spfac=8
+hypercuts=1
+compressionON=1
+binningON=1
+mergingON=1
+fineOn=1
+ruleMoveUp=0
+
 for i in acl1 ipc1 fw1
 do
     for j in 100 1K 5K 10K
@@ -10,8 +30,8 @@ do
         echo $filter
         rf="$INPUT_DIR"/"$filter".txt
         tf="$INPUT_DIR"/"$filter"_trace.txt
-        ARGS=("-r$rf" "-w$tf")
+        ARGS=("-r$rf" "-w$tf" "-b$binth" "-s$spfac" "-m$hypercuts" "-u$ruleMoveUp" "-c$compressionON" "-g$binningON" "-z$mergingON" "-F$fineOn")
         outputfile="$OUTPUT_DIR"/$filter.stat
-        ./compressedcuts ${ARGS[@]}>$outputfile
+        ./compressedcuts ${ARGS[@]}>>$outputfile
     done
 done
